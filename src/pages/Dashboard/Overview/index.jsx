f41c566dbd7e80dfc72cb7eaaeb827c548e500bf
M Sadam Sadewa <me@mssadewa.com>
2022-12-13 19:32:21 +0700

    [clean] overview page

--- src/pages/Dashboard/Overview/index.jsx
+++ src/pages/Dashboard/Overview/index.jsx
@@ -2,25 +2,8 @@ import React from 'react';
 
 import ContentContainer from 'components/Dashboard/ContentContainer';
 
-import Modal from 'components/Modal';
-
 const Overview = () => {
-  const [open, setOpen] = React.useState(false);
-
-  return (
-    <ContentContainer>
-      Overview
-      <button
-        className="btn btn-sm btn-primary m-3"
-        onClick={() => setOpen(true)}
-      >
-        Click Me
-      </button>
-      <Modal title="Test Title" open={open} handleClose={() => setOpen(false)}>
-        This is test modal
-      </Modal>
-    </ContentContainer>
-  );
+  return <ContentContainer>Overview</ContentContainer>;
 };
 
 export default Overview;
